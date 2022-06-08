b1ee3b179dcd630cae6bc655e21698492a39d85a
ralfyao <keithbogi@gmail.com>
2022-06-08 08:28:01 +0800

    加上直接從EF Set<T>()查資料，LINQ 做 JOIN功能

--- DataCentre.Api/Controllers/LoginController.cs
+++ DataCentre.Api/Controllers/LoginController.cs
@@ -23,7 +23,24 @@ namespace DataCentre.Api.Controllers
         {
             _logger.LogInfo("login user name:"+name.Username+", password:"+name.Password);
             List<LoginData> logins = _repositoryWrapper.LoginData.FindByCondition(l => l.Username == name.Username).ToList();
-            //_repositoryWrapper.GetRepositoryContext();
+            // 直接從EF Set<T>()查資料，LINQ 做 JOIN.
+            // JOIN欄位判斷 一定要 'equals'，不可以使用'=='.
+            // 以下的LINQ等同於下列SQL:
+            // SELECT `l`.`CreateDate`, `l`.`CreateUser`, `l`.`Password`, `l`.`PrivilegeGroup`,
+            // `l`.`Username`, `u`.`PrivilegeGroup`, `u`.`PrivilegeId`, `u`.`CreateDate`
+            // FROM `LoginData` AS `l`
+            // INNER JOIN `UserPrivileges` AS `u` ON `l`.`PrivilegeGroup` = `u`.`PrivilegeGroup`
+            // WHERE `l`.`Username` = '{name.Username}'
+            
+            // 取得DbSet Table做Join查詢.
+            // RepositoryContext context = _repositoryWrapper.GetRepositoryContext();
+            // 實際查詢資料.
+            // var queryResult = from user in context.Set<LoginData>()
+            //                  join privilege in context.Set<UserPrivilege>()
+            //                  on user.PrivilegeGroup equals privilege.PrivilegeGroup
+            //                  where user.Username == name.Username
+            //                  select new { user, privilege };
+
             if (logins.Count() > 0)
             {
                 _logger.LogInfo("has data");
