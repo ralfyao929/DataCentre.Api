00db0b4de117353e75d3800f2648a5b84b101344
ralfyao <keithbogi@gmail.com>
2022-06-09 15:44:59 +0800

    LoginData Add Table/Column mapping與註解

--- DataCentre.Api.Entity/Models/LoginData.cs
+++ DataCentre.Api.Entity/Models/LoginData.cs
@@ -11,13 +11,33 @@ namespace DataCentre.Api.Entity.Models
     [Table("LoginData")]
     public class LoginData
     {
+        /// <summary>
+        /// 使用者帳號
+        /// </summary>
         [Required(ErrorMessage="User Name is required")]
         [Key]
+        [Column("Username")]
         public string Username { get; set; }
+        /// <summary>
+        /// 使用者密碼
+        /// </summary>
         [Required(ErrorMessage ="Password is required")]
+        [Column("Password")]
         public string Password { get; set; }
+        /// <summary>
+        /// 所屬的權限列表，對應到 UserPrivilege
+        /// </summary>
+        [Column("PrivilegeGroup")]
         public string PrivilegeGroup { get; set; }
+        /// <summary>
+        /// 建立日期
+        /// </summary>
+        [Column("CreateDate")]
         public DateTime? CreateDate { get; set; }
+        /// <summary>
+        /// 建立人員
+        /// </summary>
+        [Column("CreateUser")]
         public string CreateUser { get; set; }
 
     }
