4d22af29bfa977b25c7141c434a458fefb1a8f6a
ralfyao <keithbogi@gmail.com>
2022-06-09 15:50:50 +0800

    Add Table/Column mapping & comments for UserPrivilege

--- DataCentre.Api.Entity/Models/UserPrivilege.cs
+++ DataCentre.Api.Entity/Models/UserPrivilege.cs
@@ -11,8 +11,20 @@ namespace DataCentre.Api.Entity.Models
     [Table("UserPrivileges")]
     public class UserPrivilege
     {
+        /// <summary>
+        /// 所屬的權限
+        /// </summary>
+        [Column("PrivilegeGroup")]
         public string PrivilegeGroup { get; set; }
+        /// <summary>
+        /// 權限的ID
+        /// </summary>
+        [Column("PrivilegeId")]
         public int PrivilegeId { get; set; }
+        /// <summary>
+        /// 權限的建立日期
+        /// </summary>
+        [Column("CreateDate")]
         public DateTime CreateDate { get; set; }
     }
 }
