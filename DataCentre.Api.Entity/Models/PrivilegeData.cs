963057b0f0d8519bf4dc67a84ff11f070ed20ee1
ralfyao <keithbogi@gmail.com>
2022-06-09 15:48:40 +0800

    PrivilegeData Add Table/Column mapping & comments

--- DataCentre.Api.Entity/Models/PrivilegeData.cs
+++ DataCentre.Api.Entity/Models/PrivilegeData.cs
@@ -11,9 +11,28 @@ namespace DataCentre.Api.Entity.Models
     [Table("PrivilegeData")]
     public class PrivilegeData
     {
+        /// <summary>
+        /// 資料PK
+        /// </summary>
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id")]
         public int Id { get; set; }
+        /// <summary>
+        /// 權限名稱
+        /// </summary>
+        [Column("PrivilegeName")]
         public string PrivilegeName { get; set; }
+        /// <summary>
+        /// 權限類型0:一般 1:報表 -1:超級管理者
+        /// </summary>
+        [Column("PrivilegeType")]
         public int PrivilegeType { get; set; }
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Column("CreatedTime")]
+        public DateTime CreatedTime { get; set; }
     }
 }
