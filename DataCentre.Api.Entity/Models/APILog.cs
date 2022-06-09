ef3992c05cf0f30f9fea173bb05dc891b261e315
ralfyao <keithbogi@gmail.com>
2022-06-09 15:41:42 +0800

    APILog Entity建立Table/Column Mapping與註解

--- DataCentre.Api.Entity/Models/APILog.cs
+++ DataCentre.Api.Entity/Models/APILog.cs
@@ -11,15 +11,47 @@ namespace DataCentre.Api.Entity.Models
     [Table("APILog")]
     public class APILog
     {
+        /// <summary>
+        /// 資料表的PK
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ID")]
         public int ID { get; set; }
+        /// <summary>
+        /// 從客端傳過來的APIUrl
+        /// </summary>
+        [Column("APIUrl")]
         public string APIUrl { get; set; }
+        /// <summary>
+        /// 客端傳遞過來的HTTP Method
+        /// </summary>
+        [Column("Method")]
         public string Method { get; set; }
+        /// <summary>
+        /// 使用者帳號
+        /// </summary>
+        [Column("User")]
         public string User { get; set; }
+        /// <summary>
+        /// 客端傳遞過來的HTTP Request的表單轉為JSON字串
+        /// </summary>
+        [Column("RequestJson")]
         public string RequestJson { get; set; }
+        /// <summary>
+        /// 回傳給客端的JSON字串
+        /// </summary>
+        [Column("ResponseJson")]
         public string ResponseJson { get; set; }
+        /// <summary>
+        /// 回傳給客端的RESPONSE CODE
+        /// </summary>
+        [Column("ResponseCode")]
         public string ResponseCode { get; set; }
+        /// <summary>
+        /// 回傳給客端的建立日期時間
+        /// </summary>
+        [Column("CreatedTime")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime CreatedTime { get; set; }
     }
